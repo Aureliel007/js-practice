715e16da1b0091d3427efcff732ad95e33ad11d4
Natalia Gribanova <nagribanova@rt-dc.ru>
2026-08-13 07:40:28 +0000

    fix force constructor

--- src/forceConstructor.js
+++ src/forceConstructor.js
@@ -1,17 +1,9 @@
-const getParamNames = function (fn) {
-    const match = fn.toString().match(/\(([^)]*)\)/);
-    if (!match || !match[1].trim()) return [];
-    return match[1].split(',').map((s) => s.trim());
-};
-
-const ForceConstructor = function (a, b, c) {
-    if (!(this instanceof ForceConstructor)) {
-        return new ForceConstructor(a, b, c);
+const ForceConstructor = function (...args) {
+    if (!new.target) {
+        return new ForceConstructor(...args);
     }
-    const paramNames = getParamNames(ForceConstructor);
-    const values = [a, b, c];
-    paramNames.forEach((name, i) => {
-        this[name] = values[i];
+    args.forEach((arg, index) => {
+        this[`arg${index + 1}`] = arg;
     });
 };
 
